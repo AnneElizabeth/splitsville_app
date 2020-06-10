c61417289e7a5d9123220f527d690d233bd11a26
AnneElizabeth <edmondson.anne@gmail.com>
2020-06-09 22:57:57 -0600

    Add custom routes for signining up and logging in.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   resources :items
   resources :lists
-  resources :users
+  resources :users, except: [:new]
+
+  #signing up
+  get '/signup', to: 'users#new'
+
+  #logging in
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/login', to: 'sessions#destroys'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
