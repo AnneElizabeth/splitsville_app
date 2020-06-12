7b80613c3aed27f7eb18698c8957d1b876b1315f
AnneElizabeth <edmondson.anne@gmail.com>
2020-06-11 22:44:27 -0600

    Begin code for ListController, CRUD actions.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,35 @@
 class ListsController < ApplicationController
+
+#create
+    def new
+        @list = List.new
+    end
+
+#read
+    def show
+        @list = List.find(params[:id])
+    end
+
+#update
+    def edit
+        @list = List.find(params[:id])
+    end
+
+    def update
+        @list = List.find(params[:id])
+        @list.update(list_params)
+        redirect_to list_path(@list)
+    end 
+
+#delete
+    def define 
+    end
+
+  private
+
+# strong_params
+  def list_params
+    params.require(:list).permit(:title, :items)
+  end
+  
 end
