1d31b6fe5c7c4c7e4d023239ecdfc4773a1011ae
AnneElizabeth <edmondson.anne@gmail.com>
2020-06-12 20:18:31 -0600

    Add code to create new user session.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,17 @@ class SessionsController < ApplicationController
         @user = User.new
     end
 
+    def create
+        @user = User.find_by(email: params[:user][:email])
+        if @user && authenticate(password: params[:user][:password])
+        # logging in
+            sessions[:user_id] = @user.id
+            redirect_to user_path(@user) # show page
+        else
+            render :new
+        end
+    end
+
     #responsible for creating a new session/logging in user
     
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,15 +4,7 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
-    def create 
-        @user = User.find_by(email: params[:user][:email])
-        if @user && @user.authenticate (password: params[:user][:password])
-            #logging in user
-            sessions[:user_id] = @user.id
-            redirect_to user_path(@user) #redirect to show page
-        else
-            render :new
-        end
+    def create
     end
 
     def show
